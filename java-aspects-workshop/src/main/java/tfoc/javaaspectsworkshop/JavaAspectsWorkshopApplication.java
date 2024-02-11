36783f5366acae9bf90d7f650ce26c7de101dee8
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-02-11 19:28:56 +0100

    started project

---
+++ java-aspects-workshop/src/main/java/tfoc/javaaspectsworkshop/JavaAspectsWorkshopApplication.java
@@ -0,0 +1,13 @@
+package tfoc.javaaspectsworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAspectsWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAspectsWorkshopApplication.class, args);
+    }
+
+}
